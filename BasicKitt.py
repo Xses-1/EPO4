c6ac9cc1e727e617eb2a739eb804e623685f6c01
Mathieu Demeestere <47038292+coolxm@users.noreply.github.com>
2024-04-24 10:53:04 +0200

    Add basicKitt Code

---
+++ BasicKitt.py
@@ -0,0 +1,33 @@
+import serial
+
+class KITT:
+    def __init__(self, port, baudrate=115200):
+        self.serial = serial.Serial(port, baudrate, rtscts=True)
+        # state variables such as speed, angle are defined here
+
+    def send_command(self, command):
+        self.serial.write(command.encode())
+
+    def set_speed(self, speed):
+        self.send_command(f'M{speed}\n')
+
+    def set_angle(self, angle):
+        self.send_command(f'D{angle}\n')
+
+    def stop(self):
+        self.set_speed(150)
+        self.set_angle(150)
+
+    def sitrep(self):
+        self.serial.write(b'S\n')
+        status = self.serial.read_until(b'\x04')
+        return status
+
+    def __del__(self):
+        self.serial.close()
+        
+if __name__ == '__main__':
+    kitt = KITT(4)
+
+    print(kitt.sitrep())
+    
