fc5aa7f6d8b3af9038288a464ecb58707ff388de
Xses <wiktort52@gmail.com>
2024-05-29 11:22:36 +0200

    Reverted to the 2D version, this one works

--- utilities/inc/tdoa.py
+++ utilities/inc/tdoa.py
@@ -1,3 +1,4 @@
+#!/bin/python3.10
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.io import wavfile
@@ -49,17 +50,6 @@ class TDOA:
         h1 = self.ch3(x, y[:, 1], Lhat, epsi)
         h2 = self.ch3(x, y[:, 2], Lhat, epsi)
         h3 = self.ch3(x, y[:, 3], Lhat, epsi)
-        h4 = self.ch3(x, y[:, 4], Lhat, epsi)
-
-        fig, ax = plt.subplots(3,2,figsize=(15,8))
-
-        ax[0,0].plot(abs(h0))
-        ax[0,1].plot(abs(h1))
-        ax[1,0].plot(abs(h2))
-        ax[1,1].plot(abs(h3))
-        ax[2,0].plot(abs(h4))
-        fig.tight_layout()
-        plt.show()
 
         tau12 = ((abs(h0).argmax() - abs(h1).argmax())*v/Fs)
         tau23 = ((abs(h1).argmax() - abs(h2).argmax())*v/Fs)
@@ -67,31 +57,101 @@ class TDOA:
         tau14 = ((abs(h0).argmax() - abs(h3).argmax())*v/Fs)
         tau13 = ((abs(h0).argmax() - abs(h2).argmax())*v/Fs)
         tau24 = ((abs(h1).argmax() - abs(h3).argmax())*v/Fs)
-        tau15 = ((abs(h0).argmax() - abs(h4).argmax())*v/Fs)
-        tau25 = ((abs(h1).argmax() - abs(h4).argmax())*v/Fs)
-        tau35 = ((abs(h2).argmax() - abs(h4).argmax())*v/Fs)
-        tau45 = ((abs(h3).argmax() - abs(h4).argmax())*v/Fs)
 
-        print(tau12,tau23,tau34,tau14,tau13,tau24,tau15,tau25,tau35,tau45)
+        print(tau12,tau23,tau34,tau14,tau13,tau24)
 
-        x1 = np.array([0, 0, 0.252])
-        x2 = np.array([0, 4.80, 0.252])
-        x3 = np.array([4.80, 4.80, 0.252])
-        x4 = np.array([4.80, 0, 0.252])
-        x5 = np.array([0, 2.40, 0.552])
+        x1 = np.array([0, 0])
+        x2 = np.array([0, 4.80])
+        x3 = np.array([4.80, 4.80])
+        x4 = np.array([4.80, 0])
 
         
 
-        A = np.array([[2*np.transpose(x2[0]-x1[0]), 2*np.transpose(x2[1]-x1[1]), 2*np.transpose(x2[2]-x1[2]),-2*tau12, 0, 0, 0],
-	                 [2*np.transpose(x3[0]-x1[0]), 2*np.transpose(x3[1]-x1[1]), 2*np.transpose(x3[2]-x1[2]),0, -2*tau13, 0, 0], 
-                     [2*np.transpose(x4[0]-x1[0]), 2*np.transpose(x4[1]-x1[1]), 2*np.transpose(x4[2]-x1[2]),0, 0, -2*tau14, 0],
-                     [2*np.transpose(x5[0]-x1[0]), 2*np.transpose(x5[1]-x1[1]), 2*np.transpose(x5[2]-x1[2]),0, 0, 0, -2*tau15], 
-                     [2*np.transpose(x3[0]-x2[0]), 2*np.transpose(x3[1]-x2[1]), 2*np.transpose(x3[2]-x2[2]),0, -2*tau23, 0, 0], 
-                     [2*np.transpose(x4[0]-x2[0]), 2*np.transpose(x4[1]-x2[1]), 2*np.transpose(x4[2]-x2[2]),0, 0, -2*tau24, 0],
-                     [2*np.transpose(x5[0]-x2[0]), 2*np.transpose(x5[1]-x2[1]), 2*np.transpose(x5[2]-x2[2]),0, 0, 0, -2*tau25], 
-                     [2*np.transpose(x4[0]-x3[0]), 2*np.transpose(x4[1]-x3[1]), 2*np.transpose(x4[2]-x3[2]),0, 0, -2*tau34, 0],
-                     [2*np.transpose(x5[0]-x3[0]), 2*np.transpose(x5[1]-x3[1]), 2*np.transpose(x5[2]-x3[2]),0, 0, 0, -2*tau35],
-                     [2*np.transpose(x5[0]-x4[0]), 2*np.transpose(x5[1]-x4[1]), 2*np.transpose(x5[2]-x4[2]),0, 0, 0, -2*tau45] ])
+        A = np.array([[ 2*(x2[0]-x1[0]).T,
+                        2*(x2[1]-x1[1]).T,
+                        -2*tau12, 0, 0],
+
+                      [ 2*(x3[0]-x1[0]).T,
+                        2*(x3[1]-x1[1]).T,
+                        0, -2*tau13, 0],
+
+                      [ 2*(x4[0]-x1[0]).T,
+                        2*(x4[1]-x1[1]).T,
+                        0, 0, -2*tau14],
+
+                      [ 2*(x3[0]-x2[0]).T,
+                        2*(x3[1]-x2[1]).T,
+                        0, -2*tau23, 0],
+
+                      [ 2*(x4[0]-x2[0]).T,
+                        2*(x4[1]-x2[1]).T,
+                        0, 0, -2*tau24],
+
+                      [ 2*(x4[0]-x3[0]).T,
+                        2*(x4[1]-x3[1]).T,
+                        0, 0, -2*tau34]])
+
+        C = np.array([  tau12**2-(np.linalg.norm(x1))**2+(np.linalg.norm(x2))**2,
+                        tau13**2-(np.linalg.norm(x1))**2+(np.linalg.norm(x3))**2,
+                        tau14**2-(np.linalg.norm(x1))**2+(np.linalg.norm(x4))**2,
+                        tau23**2-(np.linalg.norm(x2))**2+(np.linalg.norm(x3))**2,
+                        tau24**2-(np.linalg.norm(x2))**2+(np.linalg.norm(x4))**2,
+                        tau34**2-(np.linalg.norm(x3))**2+(np.linalg.norm(x4))**2])
+
+        B = np.dot(np.linalg.pinv(A), C)
+
+
+
+        '''
+        A = np.array([[ 2*np.transpose(x2[0]-x1[0]),
+                        2*np.transpose(x2[1]-x1[1]),
+                        2*np.transpose(x2[2]-x1[2]),
+                        -2*tau12, 0, 0, 0],
+
+	                 [  2*np.transpose(x3[0]-x1[0]),
+                        2*np.transpose(x3[1]-x1[1]),
+                        2*np.transpose(x3[2]-x1[2]),
+                        0, -2*tau13, 0, 0],
+
+                     [  2*np.transpose(x4[0]-x1[0]),
+                        2*np.transpose(x4[1]-x1[1]),
+                        2*np.transpose(x4[2]-x1[2]),
+                        0, 0, -2*tau14, 0],
+
+                     [  2*np.transpose(x5[0]-x1[0]),
+                        2*np.transpose(x5[1]-x1[1]),
+                        2*np.transpose(x5[2]-x1[2]),
+                        0, 0, 0, -2*tau15],
+
+                     [  2*np.transpose(x3[0]-x2[0]),
+                        2*np.transpose(x3[1]-x2[1]),
+                        2*np.transpose(x3[2]-x2[2]),
+                        0, -2*tau23, 0, 0],
+
+                     [  2*np.transpose(x4[0]-x2[0]),
+                        2*np.transpose(x4[1]-x2[1]),
+                        2*np.transpose(x4[2]-x2[2]),
+                        0, 0, -2*tau24, 0],
+
+                    [   2*np.transpose(x5[0]-x2[0]),
+                        2*np.transpose(x5[1]-x2[1]),
+                        2*np.transpose(x5[2]-x2[2]),
+                        0, 0, 0, -2*tau25],
+
+                     [  2*np.transpose(x4[0]-x3[0]),
+                        2*np.transpose(x4[1]-x3[1]),
+                        2*np.transpose(x4[2]-x3[2]),
+                        0, 0, -2*tau34, 0],
+
+                    [   2*np.transpose(x5[0]-x3[0]),
+                        2*np.transpose(x5[1]-x3[1]),
+                        2*np.transpose(x5[2]-x3[2]),
+                        0, 0, 0, -2*tau35],
+
+                     [  2*np.transpose(x5[0]-x4[0]),
+                        2*np.transpose(x5[1]-x4[1]),
+                        2*np.transpose(x5[2]-x4[2]),
+                        0, 0, 0, -2*tau45] ])
 
         C = np.array([tau12**2-(np.linalg.norm(x1))**2+(np.linalg.norm(x2))**2, 
                       tau13**2-(np.linalg.norm(x1))**2+(np.linalg.norm(x3))**2,
@@ -106,6 +166,8 @@ class TDOA:
 
         B = np.dot(np.linalg.pinv(A), C)
         #B = np.linalg.lstsq(A, C, rcond=None)[0]
+        '''
+
         return B
     
 Fs = 44100
