93b78594d52538555937f6c57886816e59922437
vigo <mathieu.demeestere04@gmail.com>
2024-06-19 22:42:59 +0200

    making plots for report

--- utilities/src/VelocityTest.py
+++ utilities/src/VelocityTest.py
@@ -7,18 +7,23 @@ import matplotlib.pyplot as plt
 
 t = np.linspace(0,20,400)
 KITT = KITTmodel()
-F_m = 7.16 #N
+F_m = 8.91 #N
 
-F = np.concatenate([[F_m] * 400])
+F = np.concatenate([[F_m] * 300, [0] * 100])
 
 V = []
+z = [0]
 
 for i in range(1,len(t)):
     dt = t[i] - t[i-1]
     v = KITT.velocity_state(F[i], dt)
-
     V.append(v)
 
-plt.plot(t[:-1],V)
+    z.append(z[-1] + v * dt)
 
+plt.plot(t,z)
+plt.hlines(z[200], 0,20, colors = 'red')
+
+print(z[300], z[399],  z[399]- z[300])
+print(V[300])
 plt.show()
